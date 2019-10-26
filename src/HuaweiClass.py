c3dc5e23cadaa3ac275c02b95315bafe20928dbc
Nikkl <niikkl@hotmail.com>
2019-10-26 02:51:15 +0200

    Update HuaweiClass.py

--- src/HuaweiClass.py
+++ src/HuaweiClass.py
@@ -1,96 +1,75 @@
-import requests, hashlib, base64, sys
+import requests, hashlib, base64
 from bs4 import BeautifulSoup
 
 class Huawei:
     # Api urls
-    ses_info_url = '/api/webserver/SesTokInfo'
-    login_url = '/api/user/login'
-    host_list_url = '/api/wlan/host-list'
-    info_url = '/api/device/information'
+    api = {
+        'ses_info': '/api/webserver/SesTokInfo',
+        'login': '/api/user/login',
+    }
+
+    # session, token, ip etc.
+    router = {
+        'ip': '',
+        'session': '',
+        'token': '',
+    }
 
     def __init__(self, ip):
-        self.ip = ip
+        self.router['ip'] = ip
         self.session = requests.Session()
         self.getToken()
 
-    # return all connected hosts
-    def listHosts(self):
-        r = self.get(self.host_list_url)
+    # request session & token
+    def getToken(self):
+        r = self.session.get('http://'+self.router['ip']+self.api['ses_info'])
         xml = BeautifulSoup(r.text, features="html.parser")
-        hosts = str(xml.response.hosts.text.strip()).replace('\n\n\n', '[HOST]').replace('\n', '[INFO]')
-        hosts = hosts.split('[HOST]')
+        self.router['token'] = xml.response.tokinfo.text
+        self.router['session'] = xml.response.sesinfo.text
 
-        for i in range(0,len(hosts)):
-            hosts[i] = hosts[i].split('[INFO]')
+    # login using token, session, user & pass
+    def login(self, user, passw):
+        passw = self.encrypt(user, passw)
 
-        return hosts
+        headers = {
+            '__RequestVerificationToken': self.router['token'],
+            'X-Requested-With': 'XMLHttpRequest',
+            'Cookie': self.router['session']
+        }
 
-    # return device information
-    def deviceInfo(self):
-        r = self.get(self.info_url)
-        xml = BeautifulSoup(r.text, features="html.parser")
-        xml = xml.response
-        print('Device Name    : '+xml.devicename.text)
-        print('Serial Number  : '+xml.serialnumber.text)
-        print('IMEI           : '+xml.imei.text)
-        print('IMSI           : '+xml.imsi.text)
-        print('Iccid          : '+xml.iccid.text)
-        print('Telephone      : '+xml.msisdn.text)
-        print('Hardware       : '+xml.hardwareversion.text)
-        print('Software       : '+xml.softwareversion.text)
-        print('WebUI          : '+xml.webuiversion.text)
-        print('MAC1           : '+xml.macaddress1.text)
-        print('MAC2           : '+xml.macaddress2.text)
-        print('Product family : '+xml.productfamily.text)
+        xml = '<?xml version="1.0" encoding="UTF-8"?><request><Username>'+user+'</Username><Password>'+passw+'</Password><password_type>4</password_type></request>'
+        r = self.session.post('http://'+self.router['ip']+self.api['login'], headers=headers, data=xml)
+        self.router['session'] = r.headers['Set-Cookie']
+
+    # generate new token & set headers
+    def updateHeaders(self):
+        self.getToken()
+        self.headers = {
+            '__RequestVerificationToken': self.router['token'],
+            'X-Requested-With': 'XMLHttpRequest',
+            'Cookie': self.router['session']
+        }
 
     # custom get/post requests
     def get(self, url, data=''):
         self.updateHeaders()
         if data != '':
-            r = self.session.get('http://'+self.ip+url, headers=self.headers, data=data)
+            r = self.session.get('http://'+self.router['ip']+url, headers=self.headers, data=data)
         else:
-            r = self.session.get('http://'+self.ip+url, headers=self.headers)
+            r = self.session.get('http://'+self.router['ip']+url, headers=self.headers)
         return r
 
-    def post(self, url, data):
+    def post(self, url, data=''):
         self.updateHeaders()
         if data != '':
-            r = self.session.post('http://'+self.ip+url, headers=self.headers, data=data)
+            r = self.session.post('http://'+self.router['ip']+url, headers=self.headers, data=data)
         else:
-            r = self.session.post('http://'+self.ip+url, headers=self.headers)
+            r = self.session.post('http://'+self.router['ip']+url, headers=self.headers)
         return r
-
-    # request session & token
-    def getToken(self):
-        r = self.session.get('http://'+self.ip+self.ses_info_url)
-        xml = BeautifulSoup(r.text, features="html.parser")
-        self.token = xml.response.tokinfo.text
-        self.session_id = xml.response.sesinfo.text
-
-    # login using token, session_id, user & pass
-    def login(self, user, passw):
-        passw = self.encrypt(user, passw)
-        headers = {
-            '__RequestVerificationToken': self.token,
-            'X-Requested-With': 'XMLHttpRequest',
-            'Cookie': self.session_id
-        }
-        xml = '<?xml version="1.0" encoding="UTF-8"?><request><Username>'+user+'</Username><Password>'+passw+'</Password><password_type>4</password_type></request>'
-        r = self.session.post('http://'+self.ip+self.login_url, headers=headers, data=xml)
-        self.auth_session = r.headers['Set-Cookie']
-
-    # generate new token & headers
-    def updateHeaders(self):
-        self.getToken()
-        self.headers = {
-            '__RequestVerificationToken': self.token,
-            'X-Requested-With': 'XMLHttpRequest',
-            'Cookie': self.auth_session
-        }
 
     # generate signature from username, password & token
     def encrypt(self, user, passw):
-        return self.base64enc(self.sha256(user+self.base64enc(self.sha256(passw))+self.token))
+        return self.base64enc(self.sha256(user+self.base64enc(self.sha256(passw))+self.router['token']))
 
     def sha256(self, s):
         sha_signature = hashlib.sha256(s.encode()).hexdigest()
