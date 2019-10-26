3299e7eb4e1dd5ae3558e50bec87a3b259d62797
Nikkl <niikkl@hotmail.com>
2019-10-26 02:57:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ api.login('username', 'password')
 # Custom POST request to turn led on
 api.post('/api/led/circle-switch', '<?xml version: "1.0" encoding="UTF-8"?><request><ledSwitch>1</ledSwitch></request>')
 
-# Custom GET request to led status
+# Custom GET request to get led status
 response = api.get('/api/led/circle-switch')
-print(response)
+print(response.text)
